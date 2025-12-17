b55b50e56da41cebb89acf6bfc07023c84bd7c07
SamInfine8 <sameer8creative.tech@gmail.com>
2025-12-17 12:49:04 +0530

    Intro to Jinja Template

--- 5.Flask/2.WebDev/app.py
+++ 5.Flask/2.WebDev/app.py
@@ -1,32 +1,10 @@
-from flask import Flask, session, render_template, request 
+from flask import Flask, session, render_template, request, Response
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template("index.html")
-
-@app.route('/login')
-def login():
-    return render_template("form.html")
-
-@app.route('/submit', methods = ['POST'])
-def submit():
-    username = request.form.get('username')
-    password = request.form.get('password')
-    '''
-    if username == "sameer8" and password=="888":
-        return render_template("welcome.html",name= username)
-    '''
-    
-    valid_users = {
-        'admin':'123',
-        'sameer8':'888',
-        'okok':'098',
-        'don': 'not_possible'
-    }
-    
-    if username in valid_users and password == valid_users[username]:
-        return render_template("welcome.html",name= username)
-    else:
-        return "In-valid User..."
+@app.route("/")
+def student_profile():
+    return render_template("profile.html", 
+                           name = "Sameer",
+                           is_topper= False,
+                           subjects=['Maths', 'Science', 'CS'])
