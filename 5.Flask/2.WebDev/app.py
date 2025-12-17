09be576c703cfb0846f823692cb75aea41cf6b0a
SamInfine8 <sameer8creative.tech@gmail.com>
2025-12-17 13:39:31 +0530

    Important Topic Template Inheritance

--- 5.Flask/2.WebDev/app.py
+++ 5.Flask/2.WebDev/app.py
@@ -8,3 +8,14 @@ def student_profile():
                            name = "Sameer",
                            is_topper= False,
                            subjects=['Maths', 'Science', 'CS'])
+    
+    
+#Inherintence (Uses- Jinja )
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
